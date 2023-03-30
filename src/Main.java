e4d071abce80c493df556d405c2be851fc876fbc
JpLew <kingdimples2@yahoo.com>
2023-03-30 19:46:19 -0400

    second commit Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
-public class Main {
+import java.io.*;
+import java.nio.file.Files;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean again = false;
+        Scanner in = new Scanner(System.in);
+
+        Worker Tom = new Worker("Tom", "Wulf", "000001", "Prof", 1960);
+        Worker Ben = new Worker("Ben", "Smith","000007","Sr.",1998);
+
+        SalaryWorker Davin = new SalaryWorker("Davin", "Harris","000009","Jr..",1996);
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+
+        System.out.println(Tom);
+        System.out.println(Tom.getFullName());
+        System.out.println(Tom.getFormalName());
+        System.out.println("Current age is " + Tom.getAge());
+        System.out.println("Age is " + Tom.getAge(1975));
+        System.out.println(Tom.toCSVRecord());
+        System.out.println(Ben.calculateWeeklyPay(41));
+        System.out.println(Ben.displayWeeklyPay(41));
+        System.out.println(Davin.displayWeeklyPay(0));
+        // System.out.println(Workers);
+
+
+
+
+
+
+
+
     }
 }
